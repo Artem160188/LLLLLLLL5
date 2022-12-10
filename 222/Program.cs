03d64ef9ae9aac06114160fcfb843f82fa057b07
Artem <118046594+Artem160188@users.noreply.github.com>
2022-12-10 21:32:07 +0300

    Add files via upload

---
+++ 222/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными
+//  числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+var rnd = new Random();
+var arr = new int[10];
+for (int i = 0; i < arr.Length; i++)
+    Console.Write("{0}\t", arr[i] = rnd.Next(1, 100));
+Console.WriteLine();
+var odd = arr.Where((element, index) => index % 2 == 0);
+Console.WriteLine("Нечётные. Количество: {0}. Сумма: {1}.", odd.Count(), odd.Sum());
+
---
+++ 333/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите 
+// разницу между максимальным и минимальным элементов массива.
+int[] myArray = { 236, 44, 66, 88, 34, 23, 34, 34, 34, 22 };
+int minimum = myArray[0];
+for (int i = 1; i < myArray.Length; i++)
+{
+    if (myArray[i] < minimum)
+    {
+        minimum = myArray[i];
+    }
+}
+
+Console.WriteLine($"Минимальное значение массива {minimum}");
+
+int maximum = myArray[0];
+for (int i = 1; i < myArray.Length; i++)
+{
+    if (myArray[i] > maximum)
+    {
+        maximum = myArray[i];
+    }
+}
+
+Console.WriteLine($"Максимальное значение массива {maximum}");
+Console.WriteLine($"Разница между максимальным и минимальным значениями массива равна {maximum - minimum}");
+
+
